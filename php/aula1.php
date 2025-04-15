3cd8af0190bcaf33e7f93a87f74ddda2d9c1a023
susananortt <norttsusana@gmail.com>
2025-04-15 17:01:29 -0300

    Create aula1.php

---
+++ php/aula1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Programa PHP</title>
+</head>
+<body>
+    <?php
+         echo "<h1>Hello World, PHP-7!</h1>";
+    ?>
+
+    <?php
+        print "teste<br>";
+        print "Olá, Mundo\n<br>";
+        print "Escape 'chars' são os MESMOS como em C\n<br>";
+        print "Você pode ter quebras de linha em uma string.<br>";
+        print 'Uma string pode usar "aspas-duplas".Isso é muito legal!<br>';
+        print 'Ainda pode-se usar aspas simples dessa forma It\'s cool<br>';
+    ?>
+
+    <?php
+        echo "<h2 align='center'>
+            O meu programa está ecoando corretamente no meu servidor PHP!
+            </h2>";
+    ?>
+
+
+
+    <h4>Susana Nort</h4>
+</body>
+</html>
