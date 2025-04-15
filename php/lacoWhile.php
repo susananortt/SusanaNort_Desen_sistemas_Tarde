e69113f7b3cdfa54865e0b20b26843e69099d82b
susananortt <norttsusana@gmail.com>
2025-04-15 17:00:57 -0300

    Create lacoWhile.php
    laço while

---
+++ php/lacoWhile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laço While</title>
+</head>
+<body>
+    <?php
+        $i = 1;
+        while ($i <=10){
+            echo $i;
+            $i++;
+        }
+    ?>
+    <h4>Susana Nort</h4>
+</body>
+</html>
