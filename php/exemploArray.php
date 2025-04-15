2957516f7b988a9ffbbe75f99551e38149b73cc7
susananortt <norttsusana@gmail.com>
2025-04-15 17:00:30 -0300

    Create exemploArray.php
    array

---
+++ php/exemploArray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo Array</title>
+</head>
+<body>
+    <?php
+        $dias = array('domingo', 'segunda','terça','quarta','quinta','sexta','sabado');
+        echo $dias[1]."<br>";
+        print_r($dias);
+        echo "<br>";
+        var_dump($dias);
+    ?>
+</body>
+</html>
