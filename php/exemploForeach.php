e9746af4d583dc29c6639092aeb91619e2c544f3
susananortt <norttsusana@gmail.com>
2025-04-16 17:00:07 -0300

    Create exemploForeach.php

---
+++ php/exemploForeach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo Foreach</title>
+</head>
+<body>
+    <?php
+        $cores = array("Amarelo", "Vermelho", "Verde", "Azul");
+        foreach($cores as $cor)
+        {
+            echo $cor."<br>";
+        }
+    ?>
+    <h4>Susana Nort</h4>
+</body>
+</html>
